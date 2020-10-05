8ac1e4fb459f28a19bca5c50959a334bbf1e5655
Jenn Liao <xl5cs@virginia.edu>
2020-10-04 22:56:28 -0400

    inital login page view

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls import url
+from django.conf.urls.static import static
 from frontend import views
 
 urlpatterns = [
-    path('', views.frontend, name='index'),
-]
+    url(r'^$', views.LoginView.as_view()),
+    url(r'^data/$', views.DataView.as_view()), # Add this /about/ route
+    url(r'^add/$', views.AddView.as_view()), # Add this /about/ route
+    # url(r'^finishreal/$', views.FinishRealPageView.as_view()), # Add this /about/ route
+    # url(r'^index_copy/$', views.IndexCopyPageView.as_view()), # Add this /about/ route
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- frontend/views.py
+++ frontend/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
-def frontend(request):
-    return render(request, 'index.html', {})
+class LoginView(TemplateView):
+    def get(self, request, **kwargs):
+        return render(request, 'login.html', context=None)
+
+# Add this view
+class DataView(TemplateView):
+    template_name = "data.html"
+
+class AddView(TemplateView):
+    template_name = "add.html"
+
+# class FinishRealPageView(TemplateView):
+#     template_name = "finishreal.html"
+
+# class IndexCopyPageView(TemplateView):
+#     template_name = "index_copy.html"
