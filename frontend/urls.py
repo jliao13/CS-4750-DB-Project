7909d71d058c8b089d1f58858e16b52ea96d89ad
Jenn Liao <xl5cs@virginia.edu>
2020-11-03 23:59:03 -0500

    fix nav bar links

--- frontend/urls.py
+++ frontend/urls.py
@@ -5,9 +5,9 @@ from django.conf.urls.static import static
 from frontend import views
 
 urlpatterns = [
-    url(r'^$', views.LoginView.as_view()),
-    url(r'^data/$', views.DataView.as_view()), # Add this /about/ route
-    url(r'^add/$', views.AddView.as_view()), # Add this /about/ route
+    url(r'^$', views.LoginView.as_view(), name='login'),
+    url(r'^data/$', views.DataView.as_view(), name='data'), # Add this /about/ route
+    url(r'^add/$', views.AddView.as_view(), name='add'), # Add this /about/ route
     # url(r'^finishreal/$', views.FinishRealPageView.as_view()), # Add this /about/ route
     # url(r'^index_copy/$', views.IndexCopyPageView.as_view()), # Add this /about/ route
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
