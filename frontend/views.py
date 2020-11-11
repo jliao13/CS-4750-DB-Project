641a43fed15c4a7899f23e14bbd6a5bb3a0a59ab
Jenn Liao <xl5cs@virginia.edu>
2020-11-11 10:25:23 -0500

    signup form works now

--- frontend/views.py
+++ frontend/views.py
@@ -3,6 +3,7 @@ from django.views.generic import TemplateView
 from .models import company, lease_tenants, manager_phone, manages
 from .forms import Loginform
 from django.http.response import HttpResponse
+from django.db import connection, transaction
 
 # Create your views here.
 class LoginView(TemplateView):
@@ -21,14 +22,38 @@ class LoginView(TemplateView):
         else:
             return render(request, 'login.html', context=None)
 
+    def create(request):
+        print("REACHED", flush=True)
+        if (request.method == "POST"):
+            user_name=request.POST['n_username']
+            password=request.POST['n_password']
+            
+            #get curr max user_id from table +1
+            cursor = connection.cursor()
+            cursor.execute("SELECT max(user_id) FROM company")
+            myresult = cursor.fetchall()
+            user_id = myresult[0][0]+1
+
+            #add new user to table
+            cursor.execute("INSERT INTO company(user_id,user_name,password) VALUES( %s , %s, %s)", [user_id, user_name, password])
+
+            try:
+                cmp = company.objects.get(user_name=user_name, password=password)
+                request.session['username'] = user_name
+                return redirect('add')
+            except:
+                return render(request, 'login.html', context=None)
+
+        return render(request, 'login.html', context=None)
+
     def logout(request):
         try:
             del request.session['username']
         except:
             pass
-        return render(request, 'login.html', context=None)
+        return redirect('login')
     
-    def get(self, request, **kwargs):              
+    def get(self, request, **kwargs):           
         return render(request, 'login.html', context=None)
 
 # Add this view
